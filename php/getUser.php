97c53c715d1ede27ae0bc13a31a7ebaba76b4422
Mukesh Mahato <mukeshmahato843@gmail.com>
2022-12-05 06:28:55 +0545

    Fixing last message

--- php/getUser.php
+++ php/getUser.php
@@ -1,52 +1,47 @@
 <?php
-    session_start();
-    include_once "connect.php";
+session_start();
+include_once "connect.php";
 
-    // if (($_SESSION['last_active_time'] - time()) >) {
-    //     # code...
-    // }
-    // echo "session id = ". $_SESSION['unique_id'];
+// if (($_SESSION['last_active_time'] - time()) >) {
+//     # code...
+// }
+// echo "session id = ". $_SESSION['unique_id'];
 
-    // echo "email = ".$_COOKIE['email']. "\n";
+// echo "email = ".$_COOKIE['email']. "\n";
 
-    if(isset($_SESSION['unique_id'])) {
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = '{$_SESSION['unique_id']}' ORDER BY active_status ");
-        // $sql = mysqli_query($conn, "SELECT * FROM users ");
-        if (mysqli_num_rows($sql) > 0) {
-            $userList = "";
+if (isset($_SESSION['unique_id'])) {
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = '{$_SESSION['unique_id']}' ORDER BY active_status ");
+    // $sql = mysqli_query($conn, "SELECT * FROM users ");
+    if (mysqli_num_rows($sql) > 0) {
+        $userList = "";
 
-            while ($data = mysqli_fetch_assoc($sql)) {
-                // echo $_SESSION['unique_id'];
-                // echo $data['unique_id'] . " ";
-                
-                // SELECT * FROM messages WHERE (incomming_id = '{$_SESSION['unique_id']}' AND outgoing_id = '{$data['unique_id']}') OR (incomming_id = '{$data['unique_id']}' AND outgoing_id = '{$_SESSION['unique_id']') ORDER BY msg_id LIMIT 1
-                    // while ($msgData = mysqli_fetch_assoc(mysqli_query($conn, "select * from messages where (outgoing_id = '{$_SESSION['unique_id']}' and incomming_id = '{$data['unique_id']}') or (outgoing_id = '{$data['unique_id']}' and incomming_id = '{$_SESSION['unique_id']}') "));) {
-                        
-                    //     echo $msgData['msg']." ";
-                    // }
-                
-                $msg = "last message";
-                    
+        while ($data = mysqli_fetch_assoc($sql)) {
+            // echo $_SESSION['unique_id'];
+            // echo $data['unique_id'] . " ";
 
-                $userList .='<a href="message.php?unique_id='. $data['unique_id'] .'">
+            // print_r($data);
+
+            $lastMsg = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `messages` WHERE incomming_id = '{$data['unique_id']}' AND outgoing_id = '{$_SESSION['unique_id']}' OR incomming_id = '{$_SESSION['unique_id']}' AND outgoing_id = '{$data['unique_id']}' ORDER by msg_id DESC LIMIT 1"))['msg'];
+
+
+            $userList .= '<a href="message.php?unique_id=' . $data['unique_id'] . '">
                                 <div class="loggedUser userList">
                                     <div>
-                                        <img src="php/uploads/'. $data['image_id'] .'">
+                                        <img src="php/uploads/' . $data['image_id'] . '">
                                         <div class="details">
-                                            <span>'. $data['fname'] ." ". $data['lname'] .'</span>
-                                            <p class="lastMsg">last message</p>
+                                            <span>' . $data['fname'] . " " . $data['lname'] . '</span>
+                                            <p class="lastMsg">' . $lastMsg . '</p>
                                         </div>
                                     </div>
                                     <div class="activeLog ' . $data['active_status'] . '"></div>
                                 </div>
                             </a>';
-            }
-            echo $userList;
-            
-        }else {
-            echo "No one is available";
+            // break;
         }
-    }else {
-        echo "error something went wrong";
+        echo $userList;
+    } else {
+        echo "No one is available";
     }
-?>
+} else {
+    echo "error something went wrong";
+}
